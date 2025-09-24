01c0158bf5a817d38e699632f707ac403af4533b
BensaltanaHoussam <bensaltna07@gmail.com>
2025-09-24 11:36:20 +0100

    add the methods signatures on the AbonnementDAOI

--- PaimentSystem/src/dao/AbonnementDAOI.java
+++ PaimentSystem/src/dao/AbonnementDAOI.java
@@ -1,5 +1,15 @@
 package dao;
 
-public interface AbonnementDAOI {
+import entities.Abonnement;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
 
+public interface AbonnementDAOI {
+    Abonnement create(Abonnement a) throws SQLException;
+    Optional<Abonnement> findById(String id) throws SQLException;
+    List<Abonnement> findAll() throws SQLException;
+    boolean update(Abonnement a) throws SQLException;
+    boolean delete(String id) throws SQLException;
+    List<Abonnement> findActiveSubscriptions() throws SQLException;
 }
