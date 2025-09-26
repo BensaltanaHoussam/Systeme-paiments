ddc66a54bafd4b87b332a5a5ab1ba26f51edf381
BensaltanaHoussam <bensaltna07@gmail.com>
2025-09-26 14:42:33 +0100

    create methods on PaimentDAOI

--- PaimentSystem/src/dao/PaiementDAOI.java
+++ PaimentSystem/src/dao/PaiementDAOI.java
@@ -1,4 +1,19 @@
 package dao;
 
+import entities.Paiement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 public interface PaiementDAOI {
+    Paiement create(Paiement p) throws SQLException;
+    Optional<Paiement> findById(String idPaiement) throws SQLException;
+    List<Paiement> findByAbonnement(String idAbonnement) throws SQLException;
+    List<Paiement> findAll() throws SQLException;
+    boolean update(Paiement p) throws SQLException;
+    boolean delete(String idPaiement) throws SQLException;
+    List<Paiement> findUnpaidByAbonnement(String idAbonnement) throws SQLException;
+    List<Paiement> findLastPayments(int limit) throws SQLException;
+    List<Paiement> findBetweenDueDates(LocalDate start, LocalDate end) throws SQLException;
 }
